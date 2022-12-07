6d75288f81daffcc01d40d9459ca2b1b0d686f37
Akash Koodali <akashkoodali9@gmail.com>
2022-12-08 00:43:48 +0530

    Created search box

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import SearchBox from "./SearchBox";
 
 function Header() {
   return (
@@ -24,6 +25,8 @@ function Header() {
       <NavLinks />
 
       {/* search box */}
+      <SearchBox />
+
     </header>
   );
 }
